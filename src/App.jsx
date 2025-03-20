93ad632ef7faefdba48fc40e5e9b37652e232293
Marcus BV <marcus.backstromviol@gmail.com>
2025-03-20 10:16:38 +0100

    style and formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import './assets/css/style.css'
-import './assets/css/animate.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, useRoutes } from "react-router-dom"
-import routesConfig from "./routing/RoutesConfig"
-import Providers from "./Providers"
+import "./assets/css/style.css";
+import "./assets/css/animate.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import routesConfig from "./routing/RoutesConfig";
+import Providers from "./Providers";
 
-function App() {
-  const routing = useRoutes(routesConfig)
-
-  return (
-    <Providers>
-      {routing} 
-    </Providers>
-  )
-}
-
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
-)
+);
 
-export default App
+function App() {
+  const routing = useRoutes(routesConfig);
+
+  return <Providers>{routing}</Providers>;
+}
+
+export default App;
